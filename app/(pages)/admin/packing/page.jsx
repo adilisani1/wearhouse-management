2f7c52e331f009f13a453364da5e1eb47d1e27bd
Adil Isani <adil.isani1@gmail.com>
2025-04-24 23:37:15 +0500

    ..

--- app/(pages)/admin/packing/page.jsx
+++ app/(pages)/admin/packing/page.jsx
@@ -50,7 +50,7 @@ const PackingPage = () => {
       </div>
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-50">
         <Suspense fallback={<div>Loading...</div>}>
-          {orderData && <PackingScreen orderData={orderData} initialFilter={filterFromUrl} />}
+          {orderData && <PackingScreen orderData={orderData} initialFilter={filterFromUrl} handlePackItem={handlePackItem} />}
         </Suspense>
       </div>
     </div>
